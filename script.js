b9a31884b3087fba68ee51ca473e27aae122ba94
Balaji-sys <balajiak008@gmail.com>
2025-08-24 19:14:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var more = document.getElementById("more");
+var involvements = document.getElementById("involvements");
+
+more.addEventListener("click",function(){
+    involvements.style.display = "block";
+})
+
