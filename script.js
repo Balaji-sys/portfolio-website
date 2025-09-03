69c0fa4c66e00b6afeb1d164393ddc9179ee4645
Balaji-sys <balajiak008@gmail.com>
2025-09-03 20:42:20 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-var more = document.getElementById("more");
-var involvements = document.getElementById("involvements");
-
-more.addEventListener("click",function(){
-    involvements.style.display = "block";
-})
-
